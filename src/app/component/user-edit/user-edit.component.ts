8b1797b81a386e32f48ccb34792aea45ad8f25bc
xiaofeng.yao <871213171@qq.com>
2017-05-27 18:01:17 +0800

    修改任务发布页样式，重写了交互

--- src/app/component/user-edit/user-edit.component.ts
+++ src/app/component/user-edit/user-edit.component.ts
@@ -7,11 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  // 页面对象
+  private userEdit:Object = {};
+
+  // 提交的表单对象
+  private SubmttObj:Object = {};
+
+  constructor() { 
+    // 初始化页面对象数据
+    this.userEdit = {
+      zIndex:8,
+      cls:[
+        {
+          name: '任'
+        },
+        {
+          name: '活'
+        }
+      ],
+      Rname:'',
+      Rdetail:'',
+      Rcount:[
+        {
+          name:'米'
+        },{
+          name:'RMB'
+        },{
+          name:'个'
+        }
+      ],
+      Rdate:'',
+      Rrule:'',
+      Rsubmit:'',
+    };
+    // 提交的页面数据对象
+    this.SubmttObj = {
+      cls: '',
+      Rname: '',
+      Rdetail: '',
+      Rcount: '',
+      Rdate: '',
+      Rrule: '',
+      Rsubmit: '',
+    }
+  }
 
   ngOnInit() {
     // 设置list高度
     this.setListHeight();
+  }
+
+  // 控制层级，递减z-index
+  reduceZIndexFun(){
+    this.userEdit['zIndex'] -= 1;
   }
 
   // 设置list高度
